409aba95251c75eea8e00309279d843cfc61d449
Lolazy <arsik.789.king@gmail.com>
2023-09-02 11:09:43 +0700

    Update

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -14,7 +14,15 @@ export default function Form({}: Props) {
     const [result, setResult] = useState('')
 
     const clearText = () => {
+        if (text == '') return
         setText('') 
+    }
+
+    const processText = () => {
+        if (text == '') return
+
+        isFormGetText(false)
+        isFormLoding(true)
     }
 
     return(
@@ -22,35 +30,46 @@ export default function Form({}: Props) {
             {formGetText && 
                 <form className="grid content-start">
                     <div className="grid grid-cols-2 text-center md:flex md:justify-between bg-black text-white rounded-t-lg">
-                            <button type="button" onClick={e => {clearText()}} className="py-4 px-0 md:px-8 hover:bg-gray-700 rounded-tl-lg">
-                                ล้างอักษร
-                            </button>
-                            <button type="button" className="py-4 px-0 md:px-8 hover:bg-gray-700 rounded-tr-lg">
-                                จัดเรียงอักษรใหม่
-                            </button>
-                        </div>
-                    <textarea name="text" id="text" value={text} rows={10} onChange={e => { setText(e.target.value)}} className="bg-gray-200 rounded-b-lg p-4 border-2 border-black resize-none mx-auto w-full outline-none"></textarea>
+                        <button type="button" onClick={e => {clearText()}} className="py-4 px-0 md:px-8 hover:bg-gray-700 rounded-tl-lg">
+                            ล้างอักษร
+                        </button>
+                        <button type="button" onClick={e => {processText()}} className="py-4 px-0 md:px-8 hover:bg-gray-700 rounded-tr-lg">
+                            จัดเรียงอักษรใหม่
+                        </button>
+                    </div>
+                    <textarea name="text" id="text" value={text} rows={15} maxLength={5000} onChange={e => { setText(e.target.value)}} className="bg-gray-200 p-4 border-x-2 border-black resize-none mx-auto w-full outline-none"></textarea>
+                    <div className="grid text-center md:text-right bg-black text-white rounded-b-lg py-1 px-4">
+                        {text.length} / 5000
+                    </div>
                 </form>
             }
+            {formLoding && 
                 <form className="grid content-start">
-                    <div className="flex justify-center py-4 bg-black text-white rounded-t-lg">
-                                กำลังประมวลผล...
-                        </div>
-                    <textarea disabled name="text" id="text" value={text} rows={10} className="bg-gray-200 rounded-b-lg p-4 border-2 border-black resize-none mx-auto w-full outline-none"></textarea>
+                    <div className="flex justify-center py-4 md:justify-end md:px-8   bg-black text-white rounded-t-lg">
+                        กำลังประมวลผล...
+                    </div>
+                    <textarea disabled name="text" id="text" value={text} rows={15} className="bg-gray-200  p-4 border-x-2 border-black resize-none mx-auto w-full outline-none"></textarea>
+                    <div className="grid text-center md:text-right bg-black text-white rounded-b-lg py-1 px-4">
+                        {text.length} / 5000
+                    </div>
                 </form>
-
+            }
+            {formResult &&
                  <form className="grid content-start">
                     <div className="grid grid-cols-2 text-center md:flex md:justify-between bg-black text-white rounded-t-lg">
-                            <button type="button" onClick={e => {clearText()}} className="py-4 px-0 md:px-8 hover:bg-gray-700 rounded-tl-lg">
-                                เริ่มต้นใหม่
-                            </button>
-                            <button type="button" className="py-4 px-0 md:px-8 hover:bg-gray-700 rounded-tr-lg">
-                                คัดลอก
-                            </button>
-                        </div>
-                    <textarea disabled name="text" id="text" value={result} rows={10} className="bg-gray-200 rounded-b-lg p-4 border-2 border-black resize-none mx-auto w-full outline-none"></textarea>
+                        <button type="button" onClick={e => {clearText()}} className="py-4 px-0 md:px-8 hover:bg-gray-700 rounded-tl-lg">
+                            เริ่มต้นใหม่
+                        </button>
+                        <button type="button" className="py-4 px-0 md:px-8 hover:bg-gray-700 rounded-tr-lg">
+                            คัดลอก
+                        </button>
+                    </div>
+                    <textarea disabled name="text" id="text" value={result} rows={15} className="bg-gray-200 p-4 border-x-2 border-black resize-none mx-auto w-full outline-none"></textarea>
+                    <div className="grid text-center md:text-right bg-black text-white rounded-b-lg py-1 px-4">
+                        {text.length} / 5000
+                    </div>
                 </form>
-
+            }
         </div>
   );
 }
