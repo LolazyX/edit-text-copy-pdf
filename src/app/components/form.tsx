2a33bfd1b6d992327168396162a246417d15dec8
Lolazy <arsik.789.king@gmail.com>
2023-09-03 18:31:25 +0700

    t

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from "react";
-import { textFormat } from "../utils/utilityFunction";
 
 type Props = {};
 
--- src/app/utils/utilityFunction.ts
+++
@@ -1,9 +0,0 @@
-
-
-export function textFormat(text: string) {
-    console.log("server is running");
-    const pattern = /[ก-ฮ]/gi
-    const txt = text.match(pattern)
-
-    return txt
-}
