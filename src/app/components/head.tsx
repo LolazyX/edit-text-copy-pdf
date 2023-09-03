e51e985c888e603b10eb48402384c0c3d27a2839
Lolazy <arsik.789.king@gmail.com>
2023-09-04 00:27:08 +0700

    update

--- src/app/components/head.tsx
+++ src/app/components/head.tsx
@@ -5,7 +5,7 @@ type Props = {};
 export default function Head({}: Props) {
   return(
     <div className="flex justify-center pb-4">
-        <h1 className="font-bold text-lg">แก้ไขตัวอักษรจาก PDF เพี้ยน</h1>
+        <h1 className="font-bold text-lg">แก้ไขตัวอักษรจาก PDF ผิดเพี้ยน</h1>
     </div>
   );
 }
